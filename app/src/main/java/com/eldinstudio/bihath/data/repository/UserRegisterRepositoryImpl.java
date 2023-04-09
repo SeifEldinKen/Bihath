17d011b7f9b9ea72cb91f9f012b08f265ec2f522
Seif Elidn <seifeldinrefaat@gmail.com>
2023-04-09 18:51:07 +0200

    ADD userRegisterWithEmailAndPassword method in UserRegisterRepositoryImpl

--- app/src/main/java/com/eldinstudio/bihath/data/repository/UserRegisterRepositoryImpl.java
+++ app/src/main/java/com/eldinstudio/bihath/data/repository/UserRegisterRepositoryImpl.java
@@ -2,13 +2,44 @@ package com.eldinstudio.bihath.data.repository;
 
 import com.eldinstudio.bihath.data.model.UserProfile;
 import com.eldinstudio.bihath.domain.repository.UserRegisterRepository;
+import com.google.firebase.auth.FirebaseAuth;
 import io.reactivex.rxjava3.core.Single;
 
 public class UserRegisterRepositoryImpl implements UserRegisterRepository {
 
+    private final FirebaseAuth firebaseAuth;
+
+    public UserRegisterRepositoryImpl(FirebaseAuth firebaseAuth) {
+        this.firebaseAuth = firebaseAuth;
+    }
+
+
+
     @Override
     public Single<UserProfile> userRegisterWithEmailAndPassword(UserProfile userProfile) {
         return Single.create( (emitter) -> {
+
+            try {
+
+                firebaseAuth.createUserWithEmailAndPassword(
+                        userProfile.getEmail(),
+                        userProfile.getPassword()
+                ).addOnSuccessListener( (authResult) -> {
+
+                    userProfile.setUserId(authResult.getUser().getUid());
+                    emitter.onSuccess(userProfile);
+
+                }).addOnFailureListener( (error) -> {
+
+                    emitter.onError(error);
+
+                });
+
+            } catch (Exception error) {
+
+                emitter.onError(error);
+
+            }
 
         });
     }
