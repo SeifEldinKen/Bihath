e7105e1a7a80bcda22197ab94263331171cb33e2
Seif Elidn <seifeldinrefaat@gmail.com>
2023-04-09 14:38:14 +0200

    Build Architecture Pattern

--- app/src/main/java/com/eldinstudio/bihath/ui/MainActivity.java
+++ app/src/main/java/com/eldinstudio/bihath/ui/MainActivity.java
@@ -1,8 +1,10 @@
-package com.eldinstudio.bihath;
+package com.eldinstudio.bihath.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.eldinstudio.bihath.R;
 
 public class MainActivity extends AppCompatActivity {
 
