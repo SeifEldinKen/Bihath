fed4354cc7b7b170e816ac349c25542ef609294d
Seif Elidn <seifeldinrefaat@gmail.com>
2023-04-10 04:19:34 +0200

    ADD FirestoreConstants singleton

---
+++ app/src/main/java/com/eldinstudio/bihath/util/FirestoreConstants.java
@@ -0,0 +1,10 @@
+package com.eldinstudio.bihath.util;
+
+public class FirestoreConstants {
+
+
+    public final static String COLLECTION_NAME_USERS = "users";
+
+    private FirestoreConstants() {}
+
+}
